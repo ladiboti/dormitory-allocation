201befcec8f969f1c65b0008b1335feaf6d7c8e6
ladiboti <ladiboti@gmail.com>
2024-10-08 00:49:57 +0200

    edit-student-modal sitebuilding complete with correct data injection

--- client/src/app/components/modals/edit-student-modal/edit-student-modal.component.ts
+++ client/src/app/components/modals/edit-student-modal/edit-student-modal.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ModalService } from '../../../services/modal.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-edit-student-modal',
@@ -11,46 +13,46 @@ export class EditStudentModalComponent implements OnInit {
   @Input() selectedStudent: any;
   editStudentForm: FormGroup;
   fields: Array<{ label: string; controlName: string; type: string; isCheckbox?: boolean }> = [
-    { label: 'Neptun kód', controlName: 'neptun', type: 'text' },
-    { label: 'Kulcs', controlName: 'key', type: 'text' },
-    { label: 'Félév', controlName: 'semester', type: 'number' },
-    { label: 'Felvételi egység', controlName: 'admissionUnit', type: 'text' },
-    { label: 'Elutasítva', controlName: 'previouslyDenied', type: 'checkbox', isCheckbox: true },
-    { label: 'Elfogadva', controlName: 'accepted', type: 'text' },
-    { label: 'Kollégiumi átlag', controlName: 'score', type: 'number' },
+    { label: 'Neptun kód', controlName: 'Neptun kód', type: 'text' },
+    { label: 'Kulcs', controlName: 'Kulcs', type: 'text' },
+    { label: 'Félév', controlName: 'Félév', type: 'number' },
+    { label: 'Felvételi egység', controlName: 'Felvételi egység', type: 'text' },
+    { label: 'Kollégiumi átlag', controlName: 'Kollégiumi átlag', type: 'number' },
   ];
 
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private modalService: ModalService, 
+    private toastrService: ToastrService
   ) {
     this.editStudentForm = this.fb.group({});
     this.fields.forEach(field => {
-      this.editStudentForm.addControl(field.controlName, this.fb.control(''));
+      this.editStudentForm.addControl(field.controlName, this.fb.control('', Validators.required));
     });
   }
 
   ngOnInit(): void {
+    console.log('Selected Student:', this.selectedStudent); 
     if (this.selectedStudent) {
       const formData: Record<string, any> = {}; 
   
       this.fields.forEach(field => {
-        if (field.controlName === 'dormitoryOrder' && Array.isArray(this.selectedStudent.dormitory_order)) {
-          formData[field.controlName] = this.selectedStudent.dormitory_order.join(', ');
-        } else {
+        if (field.controlName in this.selectedStudent) {
           formData[field.controlName] = this.selectedStudent[field.controlName] || 'Missing data';
+        } else {
+          formData[field.controlName] = 'Missing data'; 
         }
       });
   
       this.editStudentForm.patchValue(formData);
-      console.log(formData);
+      console.log('Form Data:', formData); 
     }
   }
 
   onSubmit() {
-    if (this.editStudentForm.valid) {
-      const formData = this.editStudentForm.value;
-      // formData.dormitoryOrder = formData.dormitoryOrder.split(',').map((item: string) => item.trim());
-      console.log('Form Data:', formData);
-    }
+    const formData = this.editStudentForm.value;
+    this.editStudentForm.valid
+      ? (this.modalService.closeModal(), this.toastrService.success('Sikeres módosítás!'))
+      : this.toastrService.error('Kérem minden mezőt töltsön ki!');
   }
 }
