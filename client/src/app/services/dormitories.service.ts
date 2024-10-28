f482f16548c4a5aece984ae677d25a31e766ea3d
ladiboti <ladiboti@gmail.com>
2024-10-28 01:49:40 +0100

    fetch dormitories from db implemented

---
+++ client/src/app/services/dormitories.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DormitoriesService {
+  private apiUrl = 'http://localhost:5000';
+
+  constructor(private http: HttpClient) { }
+
+  getDormitories(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/get_dormitories`); 
+  }
+
+  updateDormitory(dormitoryName: string, capacity: number) {
+    return this.http.post(`${this.apiUrl}/set_dormitories`, {
+      dormitory_name: dormitoryName,
+      capacity: capacity
+    });
+  }
+}
